774bffeb9e52918b221603b4c190dd928c97fbce
Vu Truong <vutruongqa@gmail.com>
2017-07-04 16:21:33 +0700

    add map fetching

--- app/redux-example.jsx
+++ app/redux-example.jsx
@@ -1,4 +1,5 @@
 var redux = require('redux');
+var axios = require('axios');
 console.log("Starting the redux application");
 
 // Name reducer and action generators
@@ -89,20 +90,81 @@ var removeMovie = (id) =>{
         id
     }
 };    
+// Map action generator    
+var mapReducer = (state={isFetching:false, url:undefined}, action) =>{
+    switch(action.type){
+        case 'START_LOCATION_FETCH':
+            return {
+                isFetching: true,
+                url:undefined
+            };
+        case 'COMPLETE_LOCATION_FETCH':
+            return {
+                isFetching:false,
+                url:action.url
+            };
+        default:
+            return state;
+    }
+};
+
+var startLocationFetch = () =>{
+    return {
+        type: 'START_LOCATION_FETCH'
+    };  
+};
+
+var completeLocationFetch = (url) => {
+    return {
+        type:'COMPLETE_LOCATION_FETCH',
+        url
+    };
+};
+
+var fetchLocation = () =>{
+  store.dispatch(startLocationFetch());
+      
+  var latitude = '';
+  var longtitude = '';
+  navigator.geolocation.getCurrentPosition(function(position) {
+      latitude = position.coords.latitude; 
+      longtitude = position.coords.longitude;
+      var baseUrl = 'http://maps.google.com/?q=';
+      store.dispatch(completeLocationFetch(baseUrl + latitude + ',' + longtitude));
+  });
     
+    
+//  axios.get('http://ipinfo.io').then(function(res){
+//      var loc = res.data.loc;
+//      var baseUrl = 'http://maps.google.com/?q=';
+//      store.dispatch(completeLocationFetch(baseUrl + loc));
+//  },function(){
+//      
+//  });    
+};
+
 var reducer = redux.combineReducers({
    name: nameReducer,
    hobbies: hobbiesReducer,
-   movies: moviesReducer
+   movies: moviesReducer,
+   map: mapReducer
 });
 
 var store = redux.createStore(reducer, redux.compose(window.devToolsExtension ? window.devToolsExtension() : f=>f));
 
 var unsubscribe = store.subscribe(() => {
     var state = store.getState();
-    document.getElementById('app').innerHTML = state.name;
     console.log('New state', store.getState());
+    
+    if(state.map.isFetching){
+        document.getElementById('app').innerHTML = 'Loading...';
+    }else if (state.map.url){
+        document.getElementById('app').innerHTML = '<a href="' + state.map.url + '" target="_blank">View your location</a>';
+    }
+    
 });
+
+fetchLocation();
 
 store.dispatch(changeName('Andrew'));
 
